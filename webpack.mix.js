d8f1b41ea69b7dcb90b57b78356b83b4e2f96e10
srexi <stigrex92@gmail.com>
2019-09-12 14:55:05 +0200

    1097

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,4 @@
 let mix = require('laravel-mix');
 
-mix.webpackConfig({
-    output: {
-        library: 'purecounter.js',
-        path: path.join(__dirname, 'dist'),
-        filename: path.join('[name]', 'purecounter.js'),
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    }
-}).js('js/purecounter_vanilla.js', 'dist/')
+mix.js('js/purecounter_vanilla.js', 'dist/')
     .js('purecounter.js', 'dist/');
